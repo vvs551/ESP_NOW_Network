f8e193c7d3f4a91071bdeb844af834a6bc2baabf
vvs551 <vvs551551@gmail.com>
2024-07-08 18:19:40 +0100

    Update BasicNetwork.ino

--- examples/BasicNetwork/BasicNetwork.ino
+++ examples/BasicNetwork/BasicNetwork.ino
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <time.h>
 
-#define SERVER
+#define SERVER			// !!!! Comment this line for Clients
 
 typedef struct {
   uint16_t data0;
@@ -32,12 +32,6 @@ void setup() {
 #endif
   node->onNewRecv(recv, NULL);
   srand((unsigned)time(NULL));
-}
-
-std::string dataToString(const int& value_0, const int& value_1, const int& value_2, const int& value_3) {
-  std::ostringstream oss;
-  oss << "{'DATA':[" << value_0 << "," << value_1 << "," << value_2 << "," << value_3 << "]}";
-  return oss.str();
 }
 
 void sendData() {
